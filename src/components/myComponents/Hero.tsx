4c119da9c0fddd514d8d42b157a76ff511a521e6
Ayush <ayushmehrotra099@gmail.com>
2024-12-28 15:24:27 +0530

    nothing

--- src/components/myComponents/Hero.tsx
+++ src/components/myComponents/Hero.tsx
@@ -7,18 +7,17 @@ import BoxReveal from "@/components/magicui/box-reveal";
 import Link from "next/link";
 const inter = JetBrains_Mono({ weight: "400", preload: false });
 const Hero = () => {
-
-  
-
   return (
     <>
-        <main className="flex text-white max-h-screen flex-col items-center justify-between lg:p-24 md:p-12 p-8  overflow-x-hidden  ">
+      <main className="flex text-white max-h-screen flex-col items-center justify-between lg:p-24 md:p-12 p-8  overflow-x-hidden  ">
         <BoxReveal boxColor={"#09090b"} duration={0.2}>
           <h1 className="md:text-6xl lg:text-8xl text-4xl m-4  ">
             An <span>AI</span>{" "}
           </h1>
         </BoxReveal>
-        <h1 className={`md:text-6xl lg:text-8xl text-4xl  mt-2 text-center ${inter.className}  `}>
+        <h1
+          className={`md:text-6xl lg:text-8xl text-4xl  mt-2 text-center ${inter.className}  `}
+        >
           {" "}
           <Typed />
         </h1>
@@ -31,9 +30,6 @@ const Hero = () => {
           </Button>
         </Link>
       </main>
-     
-      
-   
     </>
   );
 };

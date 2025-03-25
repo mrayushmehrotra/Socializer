0554c26ba952527516832203310d398a2c8c37fe
Ayush <ayushmehrotra099@.com>
2025-03-25 12:48:39 +0500

    changed Hero UI

--- src/components/myComponents/Hero.tsx
+++ src/components/myComponents/Hero.tsx
@@ -1,37 +1,39 @@
+"use client";
 import React from "react";
-import { Button } from "@/components/ui/button";
-import Typed from "@/Utils/Typed";
-import { FaArrowRight } from "react-icons/fa";
-import { JetBrains_Mono } from "next/font/google";
-import BoxReveal from "@/components/magicui/box-reveal";
-import Link from "next/link";
-const inter = JetBrains_Mono({ weight: "400", preload: false });
 const Hero = () => {
   return (
     <>
-      <main className="flex text-white max-h-screen flex-col items-center justify-between lg:p-24 md:p-12 p-8  overflow-x-hidden  ">
-        <BoxReveal boxColor={"#09090b"} duration={0.2}>
-          <h1 className="md:text-6xl lg:text-8xl text-4xl m-4  ">
-            An <span>AI</span>{" "}
-          </h1>
-        </BoxReveal>
-        <h1
-          className={`md:text-6xl lg:text-8xl text-4xl  mt-2 text-center ${inter.className}  `}
-        >
-          {" "}
-          <Typed />
-        </h1>
-        <Link href="/sign-up">
-          <Button
-            className="bg-white p-5  mt-12 text-black rounded-sm"
-            variant={"outline"}
-          >
-            Get Started &nbsp; <FaArrowRight />
-          </Button>
-        </Link>
+      <main>
+        <BackgroundBeamsDemo />
       </main>
     </>
   );
 };
 
 export default Hero;
+
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+export function BackgroundBeamsDemo() {
+  return (
+    <div className="h-[40rem] border border-zinc-800  w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Join the waitlist
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          Welcome to Sathi, the best CaaS (content as a service) web platform
+          that provides latest and trending services for your video to get the
+          maximum reach on all different platform&apos;s.
+        </p>
+        <input
+          type="text"
+          placeholder="hi@manuarora.in"
+          className="  px-4 py-2 rounded-full   border-zinc-800  border  focus:ring-2 focus:ring-zinc-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+        />
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
